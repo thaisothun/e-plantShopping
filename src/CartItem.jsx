8ab0189e660dd8775ba1880851275d74c54ce6d2
thaisohun <thaisothun@gmail.com>
2026-07-07 12:33:22 -0400

    update file

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -6,33 +6,37 @@ import './CartItem.css';
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
-
+  let total = ''  
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        cart.forEach(item=>{
+            let sub_toal = item.quantity * item.cost.substring(1)    
+            total = total + sub_toal
+        })
   };
 
   const handleContinueShopping = (e) => {
-    const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
-    };
+    onContinueShopping(e)
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  }
   };
 
   const handleRemove = (item) => {
-  };
+    dispatch(removeItem(item.id));
+    };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-  };
+    return item.quantity * item.cost.substring(1)
+ };
 
   return (
     <div className="cart-container">

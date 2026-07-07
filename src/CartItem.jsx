8d3f7f35afe8b078232a4afb1a8448c491a9266d
thaisohun <thaisothun@gmail.com>
2026-07-07 11:56:35 -0400

    update fuction

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -13,7 +13,9 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
+    const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+    };
   };
 
 
